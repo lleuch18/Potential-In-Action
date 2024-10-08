fac247ab5d2f702586074ed7df6780b2a7bb544b
Lasse <lasseleuchtmann@gmail.com>
2024-10-08 23:20:38 +0200

    WebScraping progressed to clicking individual button through Selenium

---
+++ Cenger_Scraper.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  8 18:28:14 2024
+
+@author: Lasse
+"""
+
+#%% Imports
+import requests
+from bs4 import BeautifulSoup
+import sys
+import time
+# #%% Cenger Scraper Raw
+# #%%% Imports
+
+
+
+# #%%% Set login_URL and login credentials for Cenger
+# login_url = 'https://www.cenger.dk/varekatalog/3dfusion-wedge-100-stk-medium-orange'
+
+
+# payload = {
+#     "user": "86820601", 
+#     "password": "torvet12",
+# }
+
+
+# #%%% Log in to Cenger
+# response = requests.post(login_url, data=payload)
+
+
+
+
+# #%%% Making a GET request
+# r = requests.get(url)
+# # check status code for response received
+# # success code - 200
+# if r.status_code == 200:
+#     print(r)
+# else: 
+#     print("faulty request")
+#     inp = input("Continue Y/N?")
+#     if inp == "N":
+#         sys.exit()
+
+
+# #%%% Test BeautifulSoup
+# # Parsing the HTML
+# soup = BeautifulSoup(r.content, 'html.parser')
+# print(soup.prettify())
+
+# #%%% Extract Data from nested HTML structure
+
+# s = soup.find('div', class_='phrase__container')
+# content = s.find_all('p')
+
+# print(s)#%% Cenger Scraper Selenium
+#%% Cenger Scraper Selenium
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
+import os
+path = os.path.join("Users","Lasse","Onedrive","Skrivebord","Cenger_2.whtt")
+# for holding the resultant list
+element_list = []
+
+
+
+
+
+
+
+page_url = "https://www.cenger.dk/varekatalog/3dfusion-wedge-100-stk-large-groen"
+# driver = webdriver.Chrome(ChromeDriverManager().install())
+driver = webdriver.Chrome()
+driver.get(page_url)
+#time.sleep(3)
+
+#Content is the main section
+content = driver.find_element(By.ID,"content1")
+
+#Locate back_button within content
+tilbage_knap = test.find_element(By.CLASS_NAME,"back")
+#Get properties of back_button
+tilbage_knap.location
+tilbage_knap.size
+#Click back_button
+#tilbage_knap.click()
+
+#Locate log_in button in row
+#cengerContainer/DesktopSection/Content1/section productView/container customContainer/row/col-md-12/row/col-md-6/cold-md-12-loginForPrices
+login_button = driver.find_element(By.XPATH,"html[1]/body[1]/div[1]/div[1]/div[3]/div[2]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]")
+
+login_button.size
+login_button.location
+login_button.click()
+
+#closing the driver
+#driver.close()
--- Library-tests.py
+++ Library-tests.py
@@ -9,7 +9,7 @@ from bs4 import BeautifulSoup
 
 
 #url = 'https://www.basiqdental.dk/da_DK/p/profylakse/tandbeskyttelse/fluor/clinpro-clear-fluoride-treatment-50pcs-l-pop-doses-mint/498461' 
-url = 'https://www.geeksforgeeks.org/python-programming-language/'
+url = 'https://www.geeksforgeeks.org/python-programming-language-tutorial/'
 
 
 #%% Standard Test 
@@ -24,22 +24,59 @@ print(r)
 print(r.content)
 
 #%% Test BeautifulSoup
-
-# Making a GET request
-r = requests.get(url)
-
-# check status code for response received
-# success code - 200
-print(r)
-
 # Parsing the HTML
 soup = BeautifulSoup(r.content, 'html.parser')
 print(soup.prettify())
 
 #%% Extract Data from nested HTML structure
 
-s = soup.find('div', class_='entry-content')
+s = soup.find('div', class_='phrase__container')
 content = s.find_all('p')
 
-print(content)
+print(s)
 
+#%% Selenium
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+# for holding the resultant list
+element_list = []
+
+for page in range(1, 3, 1):
+
+    page_url = "https://webscraper.io/test-sites/e-commerce/static/computers/laptops?page=" + str(page)
+   # driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver = webdriver.Chrome()
+    driver.get(page_url)
+    title = driver.find_elements(By.CLASS_NAME, "title")
+    price = driver.find_elements(By.CLASS_NAME, "price")
+    description = driver.find_elements(By.CLASS_NAME, "description")
+    rating = driver.find_elements(By.CLASS_NAME, "ratings")
+
+    for i in range(len(title)):
+        element_list.append([title[i].text, price[i].text, description[i].text, rating[i].text])
+
+print(element_list)
+
+#closing the driver
+driver.close()
+
+#%%% Modified Selenium Script (no loop, access scraped objects manually)
+page_url = "https://webscraper.io/test-sites/e-commerce/static/computers/laptops?page=1"
+# driver = webdriver.Chrome(ChromeDriverManager().install())
+driver = webdriver.Chrome()
+driver.get(page_url)
+title = driver.find_elements(By.CLASS_NAME, "title")
+price = driver.find_elements(By.CLASS_NAME, "price")
+description = driver.find_elements(By.CLASS_NAME, "description")
+rating = driver.find_elements(By.CLASS_NAME, "ratings")
+title[0].click()
+
+
+caption = driver.find_elements(By.CLASS_NAME,"caption")
+
+
+#closing the driver
+driver.close()
